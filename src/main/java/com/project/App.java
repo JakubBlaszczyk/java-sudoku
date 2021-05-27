9557fb3c57c93bb0bb04d92e253e02562008739f
ronbun <82181753+ronbun@users.noreply.github.com>
2021-05-27 17:28:24 +0200

    basic javaFX window

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,20 +1,22 @@
 package com.project;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
   @Override
-  public void start(Stage stage) {
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label l =
-        new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
+  public void start(Stage stage) throws IOException {
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("/fxml/test.fxml"));
+    Parent root = loader.load();
+    Scene scene = new Scene(root, 300, 275);
+
+    stage.setTitle("FXML Welcome");
     stage.setScene(scene);
     stage.show();
   }

fe42e795ee897df239d969dda7c1c36a8c73c775
Jakub <jakubus118@gmail.com>
2021-05-27 18:50:54 +0200

    Solver done #1

--- src/main/java/com/project/Board.java
+++ src/main/java/com/project/Board.java
@@ -24,4 +24,7 @@ public class Board {
     public int getTileValue(int x, int y) {
         return tilesValue.get(x * tilesValue.size() + y).intValue();
     }
+    public int setTileValue(int x, int y, int value) {
+        return tilesValue.set(x * tilesValue.size() + y, Integer.valueOf(value));
+    }
 }
--- src/main/java/com/project/Sudoku.java
+++ src/main/java/com/project/Sudoku.java
@@ -6,31 +6,68 @@ public class Sudoku {
     private Board board;
     private ArrayList<Integer> tilesLogic;
     private ArrayList<Boolean> tilesPossibilities;
+    private ArrayList<Board> sudokusToCome;
 
     public Sudoku(Board board) {
         this.board = board;
-        tilesLogic = new ArrayList<>(this.board.getSize() * this.board.getSize());
-        tilesPossibilities = new ArrayList<>(this.board.getSize() * this.board.getSize() * this.board.getSize());
-        for (int i = 0; i < this.tilesPossibilities.size(); ++i) {
-            this.tilesPossibilities.set(i, true);
+        sudokusToCome = new ArrayList<>();
+        tilesLogic = new ArrayList<>(board.getSize() * board.getSize());
+        tilesPossibilities = new ArrayList<>(board.getSize() * board.getSize() * board.getSize());
+        for (int i = 0; i < tilesPossibilities.size(); ++i) {
+            tilesPossibilities.set(i, true);
         }
-        for (int i = 0; i < this.tilesLogic.size(); ++i) {
-            this.tilesLogic.set(i, this.board.getSize());
+        for (int i = 0; i < tilesLogic.size(); ++i) {
+            tilesLogic.set(i, board.getSize());
         }
     }
 
     public void solve() {
-        // TODO: real loop
-        while (this.isSolvable()) {
-            this.updateLogic();
+        while (true) {
+            updateLogic();
+            if (isSolvable()) {
+                int currentIndex = findSmallest();
+
+                int x = currentIndex / board.getSize();
+                int y = currentIndex - x * board.getSize();
+                if (tilesLogic.get(currentIndex) == 1) {
+                    board.setTileValue(x, y, fetchNumber(x, y));
+                }
+                else if (tilesLogic.get(currentIndex) > 1) {
+                    int j = 0;
+                    for (int i = 1; i < tilesLogic.get(currentIndex); ++i) {
+                        for (; j < board.getSize(); ++j) {
+                            if (tilesPossibilities
+                                    .get(x * board.getSize() * board.getSize() + y * board.getSize() + j) == true) {
+                                Board tempBoard = this.board;
+                                tempBoard.setTileValue(x, y, j + 1);
+                                sudokusToCome.add(tempBoard);
+                                ++j;
+                                break;
+                            }
+                        }
+                    }
+                    for (; j < board.getSize(); ++j) {
+                        if (tilesPossibilities
+                                .get(x * board.getSize() * board.getSize() + y * board.getSize() + j) == true) {
+                            board.setTileValue(x, y, j + 1);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                // TODO fetch next sudoku to solve
+                if (!sudokusToCome.isEmpty()) {
+                    board = sudokusToCome.remove(sudokusToCome.size() - 1);
+                }
+            }
         }
     }
 
     private boolean isSolvable() {
-        for (int i = 0; i < this.board.getSize(); ++i) {
-            for (int j = 0; j < this.board.getSize(); ++j) {
-                if (this.board.getTileValue(i, j) == 0) {
-                    if (this.tilesLogic.get(i * this.board.getSize() + j) == 0) {
+        for (int i = 0; i < board.getSize(); ++i) {
+            for (int j = 0; j < board.getSize(); ++j) {
+                if (board.getTileValue(i, j) == 0) {
+                    if (tilesLogic.get(i * board.getSize() + j) == 0) {
                         return false;
                     }
                 }
@@ -40,24 +77,24 @@ public class Sudoku {
     }
 
     private void updateLogic() {
-        this.FilledUpdate();
-        for (int i = 0; i < this.board.getSize(); ++i) {
-            for (int j = 0; j < this.board.getSize(); ++j) {
-                this.RowLogic(i, j);
-                this.ColumnLogic(i, j);
-                this.BoxLogic(i, j);
+        FilledUpdate();
+        for (int i = 0; i < board.getSize(); ++i) {
+            for (int j = 0; j < board.getSize(); ++j) {
+                RowLogic(i, j);
+                ColumnLogic(i, j);
+                BoxLogic(i, j);
             }
         }
     }
 
     private void FilledUpdate() {
-        for (int i = 0; i < this.board.getSize(); ++i) {
-            for (int j = 0; j < this.board.getSize(); ++j) {
-                if (this.board.getTileValue(i, j) != 0) {
-                    int posInPossibilities = i * this.board.getSize() * this.board.getSize() + j * this.board.getSize();
-                    this.tilesLogic.set(i * this.board.getSize() + j, Integer.valueOf(0));
-                    for (int k = 0; k < this.board.getSize(); ++k) {
-                        this.tilesPossibilities.set(posInPossibilities + k, false);
+        for (int i = 0; i < board.getSize(); ++i) {
+            for (int j = 0; j < board.getSize(); ++j) {
+                if (board.getTileValue(i, j) != 0) {
+                    int posInPossibilities = i * board.getSize() * board.getSize() + j * board.getSize();
+                    tilesLogic.set(i * board.getSize() + j, Integer.valueOf(0));
+                    for (int k = 0; k < board.getSize(); ++k) {
+                        tilesPossibilities.set(posInPossibilities + k, false);
                     }
                 }
             }
@@ -66,37 +103,37 @@ public class Sudoku {
 
     private void RowLogic(int x, int y) {
         // if it needs refreshing breeze through line
-        if (this.board.getTileValue(x, y) == 0) {
-            int posInPossibilities = x * this.board.getSize() * this.board.getSize() + y * this.board.getSize();
-            for (int i = 0; i < this.board.getSize(); ++i) {
-                if (this.board.getTileValue(i, y) != 0) {
-                    this.tilesPossibilities.set(posInPossibilities + this.board.getTileValue(i, y) - 1, true);
+        if (board.getTileValue(x, y) == 0) {
+            int posInPossibilities = x * board.getSize() * board.getSize() + y * board.getSize();
+            for (int i = 0; i < board.getSize(); ++i) {
+                if (board.getTileValue(i, y) != 0) {
+                    tilesPossibilities.set(posInPossibilities + board.getTileValue(i, y) - 1, true);
                 }
             }
         }
     }
 
     private void ColumnLogic(int x, int y) {
-        if (this.board.getTileValue(x, y) == 0) {
-            int posInPossibilities = x * this.board.getSize() * this.board.getSize() + y * this.board.getSize();
-            for (int i = 0; i < this.board.getSize(); ++i) {
-                if (this.board.getTileValue(x, i) != 0) {
-                    this.tilesPossibilities.set(posInPossibilities + this.board.getTileValue(x, i) - 1, true);
+        if (board.getTileValue(x, y) == 0) {
+            int posInPossibilities = x * board.getSize() * board.getSize() + y * board.getSize();
+            for (int i = 0; i < board.getSize(); ++i) {
+                if (board.getTileValue(x, i) != 0) {
+                    tilesPossibilities.set(posInPossibilities + board.getTileValue(x, i) - 1, true);
                 }
             }
         }
     }
 
     private void BoxLogic(int x, int y) {
-        if (this.board.getTileValue(x, y) == 0) {
+        if (board.getTileValue(x, y) == 0) {
             int tempX, tempY;
-            int posInPossibilities = x * this.board.getSize() * this.board.getSize() + y * this.board.getSize();
-            tempX = (x / this.board.getBoxWidth()) * this.board.getBoxWidth();
-            tempY = (y / this.board.getBoxHeight()) * this.board.getBoxHeight();
-            for (int i = tempX; i < tempX + this.board.getBoxWidth(); ++i) {
-                for (int j = tempX; j < tempY + this.board.getBoxHeight(); ++j) {
-                    if (this.board.getTileValue(i, j) == 0) {
-                        this.tilesPossibilities.set(posInPossibilities + this.board.getTileValue(i, j) - 1, true);
+            int posInPossibilities = x * board.getSize() * board.getSize() + y * board.getSize();
+            tempX = (x / board.getBoxWidth()) * board.getBoxWidth();
+            tempY = (y / board.getBoxHeight()) * board.getBoxHeight();
+            for (int i = tempX; i < tempX + board.getBoxWidth(); ++i) {
+                for (int j = tempX; j < tempY + board.getBoxHeight(); ++j) {
+                    if (board.getTileValue(i, j) == 0) {
+                        tilesPossibilities.set(posInPossibilities + board.getTileValue(i, j) - 1, true);
                     }
                 }
             }
@@ -104,17 +141,26 @@ public class Sudoku {
     }
 
     private int findSmallest() {
-        int smallest = this.board.getSize() + 1;
+        int smallest = board.getSize() + 1;
         int index = -1;
-        for (int i = 0; i < this.board.getSize(); ++i) {
-            for (int j = 0; j < this.board.getSize(); ++j) {
-                if(this.tilesLogic.get(i * this.board.getSize() + j) != 0 && this.tilesLogic.get(i * this.board.getSize() + j) < smallest)
-                {
-                    smallest = this.tilesLogic.get(i * this.board.getSize() + j);
-                    index = i * this.board.getSize() + j;
+        for (int i = 0; i < board.getSize(); ++i) {
+            for (int j = 0; j < board.getSize(); ++j) {
+                if (tilesLogic.get(i * board.getSize() + j) != 0
+                        && tilesLogic.get(i * board.getSize() + j) < smallest) {
+                    smallest = tilesLogic.get(i * board.getSize() + j);
+                    index = i * board.getSize() + j;
                 }
             }
         }
         return index;
+    }
+
+    private int fetchNumber(int x, int y) {
+        for (int i = 0; i < board.getSize(); ++i) {
+            if (tilesPossibilities.get(x * board.getSize() * board.getSize() + y * board.getSize() + i)) {
+                return i + 1;
+            }
+        }
+        return 0;
     }
 }

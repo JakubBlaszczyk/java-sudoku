779e83eaa12a5d53ee554cf46fae97ca56a3e6f7
ronbun <82181753+ronbun@users.noreply.github.com>
2021-06-07 17:30:14 +0200

    button select && change value

--- src/main/java/com/project/Controller.java
+++ src/main/java/com/project/Controller.java
@@ -1,49 +1,112 @@
 package com.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import java.io.IOException;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import  javafx.scene.control.Button;
 
 public class Controller {
-    @FXML
-    private Button Button00; // Id 00,01,02...-55
-    private Button Set1;
-    private  boolean selected = false;
-    BackgroundFill Red = new BackgroundFill(Color.RED, new CornerRadii(1),
-        new Insets(0.0,0.0,0.0,0.0));
-    BackgroundFill White = new BackgroundFill(Color.WHITE, new CornerRadii(1),
-        new Insets(0.0,0.0,0.0,0.0));
+  @FXML
+  private Button Button00; // Id 00,01,02...-55
 
-    public void ChangeValue(ActionEvent event){
+  BackgroundFill red = new BackgroundFill(Color.RED, new CornerRadii(1), new Insets(0.0, 0.0, 0.0, 0.0));
+  BackgroundFill white = new BackgroundFill(Color.WHITE, new CornerRadii(1), new Insets(0.0, 0.0, 0.0, 0.0));
+  Object lastClickedButton = null;
 
-    Button00.setBackground(new Background(Red));
-    selected = true;
+  @FXML
+  private GridPane sudokuBoard;
+  @FXML
+  private HBox setButtons;
 
+  @FXML
+  public void initialize() {
+    // setup
+    setupSudokuButtons();
+    setupNumberButtons();
+  }
+
+  private void setupSudokuButtons() {
+    EventHandler<ActionEvent> handler = ev -> {
+      Button button = ((Button) ev.getSource());
+      if (button.getBackground().equals(new Background(red))) {
+        button.setBackground(new Background(white));
+      } else {
+        button.setBackground(new Background(red));
+      }
+    };
+
+    for (Node node : sudokuBoard.getChildren()) {
+      if (!(node instanceof Pane)) {
+        throw new IllegalArgumentException("Missing Pane in pane grid!");
+      }
+      Button button = (Button) (((Pane) node).getChildren().get(0));
+      button.setOnAction(handler);
     }
-    public void SetValue1(ActionEvent event) {
-        if (selected==false) {
-            // prompt wybieraj cos
-        } else {
-            for (int i = 0; i < 6;i++) {
-                for (int j = 0; j < 6;j++) {
-                    if (i==j) {
-                        Button00.setText("1");
-                        Button00.setBackground(new Background(White));
-                        selected = false;
-                    }
+  }
 
-                }
-            }
+  private void setupNumberButtons() {
+    EventHandler<MouseEvent> handler = ev -> {
+      int clicked = 0;
+      for (Node node : sudokuBoard.getChildren()) {
+        if (!(node instanceof Pane)) {
+          throw new IllegalArgumentException("Missing Pane in pane grid!");
         }
+        Button button = (Button) (((Pane) node).getChildren().get(0));
+        if (button.getBackground().equals(new Background(red))) {
+          ++clicked;
+          button.setText(((Button) ev.getSource()).getText());
+          button.setBackground(new Background(white));
+        }
+      }
+      if (clicked == 0) {
+        // TODO add popup
+      }
+    };
+
+    for (Node node : setButtons.getChildren()) {
+      if (!(node instanceof Button)) {
+        throw new IllegalArgumentException("Not Button in button grid!");
+      }
+      ((Button) node).setOnMouseClicked(handler);
     }
+  }
+
+  public void setSudokuButtonValue(ActionEvent ev) {
+    Button button = ((Button) ev.getSource());
+    if (button.getBackground().equals(new Background(red))) {
+      button.setBackground(new Background(white));
+    } else {
+      button.setBackground(new Background(red));
+    }
+  }
+
+  public void onClickSudokuNumber(MouseEvent ev) {
+    int clicked = 0;
+    for (Node node : sudokuBoard.getChildren()) {
+      if (!(node instanceof Pane)) {
+        throw new IllegalArgumentException("Missing Pane in pane grid!");
+      }
+      Button button = (Button) (((Pane) node).getChildren().get(0));
+      if (button.getBackground().equals(new Background(red))) {
+        ++clicked;
+        button.setText(((Button) ev.getSource()).getText());
+      }
+    }
+    if (clicked == 0) {
+      // TODO add popup
+    }
+  }
 }

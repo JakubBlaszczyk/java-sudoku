9d4b9fc80f4868e1caaddda70887ba17a7753c29
ronbun <82181753+ronbun@users.noreply.github.com>
2021-06-10 00:09:53 +0200

    Unit test for Sudoku.Hint()

--- src/test/java/com/project/AppTest.java
+++ src/test/java/com/project/AppTest.java
@@ -3,16 +3,29 @@ package com.project;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-/**
- * Unit test for simple App.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.project.boards.Board6x6;
+import com.project.exceptions.SudokuAlreadySolved;
+import com.project.exceptions.SudokuUnsolvable;
+
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
-    }
+  @Test
+  void testHint() throws SudokuAlreadySolved, SudokuUnsolvable {
+    assertThrows(SudokuAlreadySolved.class, () -> {
+      ArrayList<Integer> vals = new ArrayList<>(Arrays.asList(4, 5, 1, 2, 3, 6, 2, 6, 5, 3, 1, 4, 3, 1, 4, 6, 2, 5, 5,
+          3, 2, 4, 6, 1, 1, 2, 6, 5, 4, 3, 6, 4, 3, 1, 5, 2));
+      Board board = new Board6x6(vals);
+      Sudoku.hint(board);
+    });
+    ArrayList<Integer> vals = new ArrayList<>(Arrays.asList(0, 5, 1, 2, 3, 6, 2, 6, 5, 3, 1, 4, 3, 1, 4, 6, 2, 5, 5, 3,
+        2, 4, 6, 1, 1, 2, 6, 5, 4, 3, 6, 4, 3, 1, 5, 2));
+    Board board = new Board6x6(vals);
+    Hint hint = Sudoku.hint(board);
+    Hint corretHint = new Hint(0, 0, 4);
+    assertEquals(corretHint, hint);
+  }
 }

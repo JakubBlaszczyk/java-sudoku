207a6924fe28647db8dedb4bf36e62c7bbc06768
ronbun <82181753+ronbun@users.noreply.github.com>
2021-06-09 21:53:19 +0200

    fix loading Board from file

--- src/main/java/com/project/Board.java
+++ src/main/java/com/project/Board.java
@@ -1,5 +1,10 @@
 package com.project;
 
+import com.project.boards.Board10x10;
+import com.project.boards.Board12x12;
+import com.project.boards.Board6x6;
+import com.project.boards.Board8x8;
+import com.project.boards.Board9x9;
 import com.project.exceptions.InvalidSudokuData;
 import com.project.exceptions.InvalidSudokuSize;
 import java.io.BufferedReader;
@@ -42,10 +47,40 @@ public class Board {
     return tilesValue.set(x * tilesValue.size() + y, Integer.valueOf(value));
   }
 
-  // Each exception should have it's own message (GUI stuff)
-  public void loadBoard(String filePath) throws IOException, InvalidSudokuData, InvalidSudokuSize {
-    tilesValue.clear();
+  public List<Integer> getTilesValue() {
+    return tilesValue;
+  }
 
+  public void setTilesValue(List<Integer> tilesValue) {
+    this.tilesValue = tilesValue;
+  }
+
+  // Each exception should have it's own message (GUI stuff)
+  public static Board loadBoard(String filePath) throws IOException, InvalidSudokuData, InvalidSudokuSize {
+    List<String> nums = loadArray(filePath);
+    if (!checkIfStringIsParsable(nums)) {
+      log.info("Sudoku data contains not parsable characters");
+      throw new InvalidSudokuData();
+    }
+    Board board = createBoard(nums);
+    board.tilesValue.clear();
+    for (int i = 0; i < nums.size(); ++i) {
+      board.tilesValue.add(0);
+    }
+    int arrSize = board.getSize();
+
+    log.debug("Before trans: {}", nums);
+    // Transform cols into rows
+    for (int i = 0; i < arrSize; ++i) {
+      for (int j = 0; j < arrSize; ++j) {
+        board.tilesValue.set(i * arrSize + j, Integer.parseInt(nums.get(i + j * arrSize)));
+      }
+    }
+    log.debug("After trans: {}", board.tilesValue);
+    return board;
+  }
+
+  private static List<String> loadArray(String filePath) throws InvalidSudokuSize, IOException {
     BufferedReader reader;
     reader = new BufferedReader(new FileReader(filePath));
     List<String> nums = new ArrayList<>();
@@ -54,35 +89,35 @@ public class Board {
       if (line == null) {
         break;
       }
-      nums.addAll(Arrays.asList(line.split(" ")));
+      line = line.trim();
+      nums.addAll(Arrays.asList(line.split("\\s+")));
     }
     reader.close();
 
     double testSize = Math.sqrt(nums.size());
-
     if (!((testSize == Math.floor(testSize)) && !Double.isInfinite(testSize))) {
-      log.info("Sudoku size has to be square value, size = {}", testSize);
+      log.warn("Sudoku size has to be square value, size = {}", testSize);
       throw new InvalidSudokuSize();
     }
-    int arrSize = (int) testSize;
-    
-    if (!checkIfStringIsParsable(nums)) {
-      log.info("Sudoku data contains not parsable characters");
-      throw new InvalidSudokuData();
-    }
+    return nums;
+  }
 
-    for (int i = 0; i < nums.size(); ++i) {
-      tilesValue.add(0);
+  private static Board createBoard(List<String> list) throws InvalidSudokuSize {
+    switch ((int)Math.sqrt(list.size())) {
+      case 6:
+        return new Board6x6(new ArrayList<>());
+      case 8:
+        return new Board8x8(new ArrayList<>());
+      case 9:
+        return new Board9x9(new ArrayList<>());
+      case 10:
+        return new Board10x10(new ArrayList<>());
+      case 12:
+        return new Board12x12(new ArrayList<>());
+      default:
+        log.warn("Sudoku size not within valid range, {}", list.size());
+        throw new InvalidSudokuSize();
     }
-    
-    log.debug("Before trans: {}", tilesValue);
-    // Transform cols into rows
-    for (int i = 0; i < arrSize; ++i) {
-      for (int j = 0; j < arrSize; ++j) {
-        tilesValue.set(i * arrSize + j, Integer.parseInt(nums.get(i + j * arrSize)));
-      }
-    }
-    log.debug("After trans: {}", tilesValue);
   }
 
   // Data have to be colleded by rows
@@ -101,7 +136,7 @@ public class Board {
     wr.close();
   }
 
-  private boolean checkIfStringIsParsable(List<String> strNums) {
+  private static boolean checkIfStringIsParsable(List<String> strNums) {
     try {
       for (String num : strNums) {
         Integer.parseInt(num);

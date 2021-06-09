e415c7bda47ffa9b2a23be5149a82bf7114c19b8
ronbun <82181753+ronbun@users.noreply.github.com>
2021-06-09 21:50:00 +0200

    add encapsulation

--- src/main/java/com/project/Tuple.java
+++ src/main/java/com/project/Tuple.java
@@ -1,13 +1,37 @@
 package com.project;
 
 public class Tuple {
-    public int x;
-    public int y;
-    public int value;
+  private int x;
+  private int y;
+  private int value;
 
-    public Tuple(int x, int y, int value) {
-        this.x = x;
-        this.y = y;
-        this.value = value;
-    }
+  public Tuple(int x, int y, int value) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
 }
